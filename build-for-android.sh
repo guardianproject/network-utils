e41832e44b25eb04e9d5469260b0a75f28e3da6a
Hans-Christoph Steiner <hans@eds.org>
2011-02-14 01:53:01 -0500

    got tcptraceroute building as static

--- build-for-android.sh
+++ build-for-android.sh
@@ -6,6 +6,11 @@
 #   git submodule init
 #   git submodule update
 
+# TODO add sha1 checking to all downloads
+# TODO download tcptraceroute
+# TODO make patch for tcptraceroute configure.ac
+# TODO push to guardianproject git
+
 CWD=`pwd`
 LIB_ROOT=$CWD/root
 
@@ -21,11 +26,23 @@ NDK_TOOLCHAIN=${NDK_BASE}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/${NDK_
 HOST=arm-linux-androideabi
 
 CC="$NDK_TOOLCHAIN/bin/${HOST}-gcc --sysroot=$NDK_SYSROOT"
+RANLIB="$NDK_TOOLCHAIN/bin/${HOST}-ranlib"
 
 CFLAGS="-I${LIB_ROOT}/include"
 LDFLAGS="-L${LIB_ROOT}/lib"
 
 #------------------------------------------------------------------------------#
+# download
+
+wget http://sourceforge.net/projects/libnet-dev/files/libnet-1.1.5.tar.gz/download
+tar xzf libnet-1.1.5.tar.gz
+
+wget http://www.tcpdump.org/release/libpcap-1.1.1.tar.gz
+tar xzf libpcap-1.1.1.tar.gz
+
+
+#------------------------------------------------------------------------------#
+# build 
 
 mkdir $LIB_ROOT    
 
@@ -33,15 +50,28 @@ echo "----------------------------------------"
 echo "libnet"
 cd ${CWD}/libnet-1.1.5
 cp ../config.guess ../config.sub .
-./configure CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=${HOST} --enable-shared --enable-static
+./configure CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=${HOST} --prefix= --enable-shared --enable-static
 make
 make DESTDIR=$LIB_ROOT prefix= install
+$RANLIB ${LIB_ROOT}/lib/libnet.a
 
 echo "----------------------------------------"
 echo "libpcap-1.1.1"
 cd ${CWD}/libpcap-1.1.1
 cp ../config.guess ../config.sub .
-ac_cv_linux_vers=2 ./configure CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=${HOST} --enable-shared --enable-static --with-pcap=linux
+ac_cv_linux_vers=2 ./configure CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=${HOST} --prefix= --enable-shared --enable-static --with-pcap=linux
+make
+make DESTDIR=$LIB_ROOT prefix= install
+$RANLIB ${LIB_ROOT}/lib/libpcap.a
+
+echo "----------------------------------------"
+echo "tcptraceroute-1.5beta7"
+cd ${CWD}/tcptraceroute-1.5beta7
+cp ../config.guess ../config.sub .
+autoreconf
+automake --add-missing
+export ac_cv_func_malloc_0_nonnull=yes 
+./configure CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --host=${HOST} --prefix= --enable-static --with-libnet=${LIB_ROOT} --with-libpcap=${LIB_ROOT}
 make
 make DESTDIR=$LIB_ROOT prefix= install
 
